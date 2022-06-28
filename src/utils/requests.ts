0626970f79b18e9fff6b005082a5588545778650
lucaskrispim <lucaskrispim@hotmail.com>
2022-06-28 11:40:24 -0300

    to fixed

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://127.0.0.1:8000';
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://127.0.0.1:8000';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://django-dividends.herokuapp.com';
