d4a98678e5d028c6722b1669da85ca6882ba9f20
lucaskrispim <lucaskrispim@hotmail.com>
2022-07-11 23:02:56 -0300

    disable button

--- src/components/navigationrow/index.tsx
+++ src/components/navigationrow/index.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom"
 
 const Home = () => {
+
+  const getTo = (toName: string) => {
+    return window.location.pathname === toName ? true : false
+  }
+
+
   return (
     <>
       <div className="card mb-2">
         <div className="card-body">      
-              <Link className="d-inline btn btn-outline-dark m-2" to="/magic">Magic Formula</Link>
+              <Link className={getTo('/magic') ?  'd-inline btn btn-dark m-2': 'd-inline btn btn-outline-dark m-2' } to={getTo('/magic') ?  '#': '/magic' }>Magic Formula</Link>
             
-              <Link className="d-inline  btn btn-outline-dark m-2" to="/">Dividends</Link>
+              <Link className={getTo('/') ?  'd-inline btn btn-dark m-2': 'd-inline btn btn-outline-dark m-2' } to={getTo('/') ? '#':'/'}>Dividends</Link>
           
         </div>
       </div>
